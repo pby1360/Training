77417b817b57597aff6c6d38880c2ae0195b6005
pby1360 <pby1360@gmail.com>
2020-03-31 08:37:58 +0900

    done : id 지정 , toDo : 클릳이벤트

--- app/src/main/java/com/example/training/StopwatchActivity.java
+++ app/src/main/java/com/example/training/StopwatchActivity.java
@@ -3,12 +3,25 @@ package com.example.training;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
 
 public class StopwatchActivity extends AppCompatActivity {
+	
+	private TextView tv_sw_time;
+	private ImageView iv_sw_reset;
+	private ImageView iv_sw_start;
+	private ImageView iv_sw_stop;
+	private Button btn_sw_record;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+		
+		tv_sw_time = findViewById(R.id.tv_sw_time);
+		iv_sw_start = findViewById(R.id.iv_sw_start);
+		iv_sw_reset = findViewById(R.id.iv_sw_reset);
+		iv_sw_stop = findViewById(R.id.iv_sw_stop);
+		btn_sw_record = findViewById(R.id.btn_sw_record);
     }
 }
