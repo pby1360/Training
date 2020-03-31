6f02d5bd275d5d2b3368f5ac53b1ae1935ad92a5
pby1360 <pby1360@gmail.com>
2020-04-01 08:53:16 +0900

    commit

--- app/src/main/java/com/example/training/StopwatchActivity.java
+++ app/src/main/java/com/example/training/StopwatchActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.*;
 import android.os.SystemClock;
 import android.view.View.*;
+import android.os.Handler;
+import android.os.Message;
 
 public class StopwatchActivity extends AppCompatActivity {
 	
@@ -31,14 +33,23 @@ public class StopwatchActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     myStartTime = SystemClock.elapsedRealtime();
+					myTimer.sendEmptyMessage(0);
+					iv_sw_start.setImageResource(R.drawable.icon_pause_custom);
                     
                 }
          });
         
     }
-    String getTimeOut() {
+    
+	Handler myTimer = new Handler() {
+		public void handleMessage(Message msg) {
+			tv_sw_time.setText(getTimeOut());
+			myTimer.sendEmptyMessage(0);
+		}
+	};
+	String getTimeOut() {
         long now = SystemClock.elapsedRealtime();
-        long outTime = now - myStartTime);
+        long outTime = now - myStartTime;
         String time = String.format("%02d : %02d : %02d", (outTime / 1000 / 3600), (outTime / 1000 / 60) % 60, (outTime / 1000) % 60);
         return time;
 	}
