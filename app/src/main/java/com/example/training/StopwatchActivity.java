8945322b675f448bef437d7b50e780d1b34cb7a0
pby1360 <pby1360@gmail.com>
2020-04-01 08:38:37 +0900

    toDo stopwatch java

--- app/src/main/java/com/example/training/StopwatchActivity.java
+++ app/src/main/java/com/example/training/StopwatchActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.*;
+import android.os.SystemClock;
+import android.view.View.*;
 
 public class StopwatchActivity extends AppCompatActivity {
 	
@@ -12,6 +14,7 @@ public class StopwatchActivity extends AppCompatActivity {
 	private ImageView iv_sw_start;
 	private ImageView iv_sw_stop;
 	private Button btn_sw_record;
+    private long myStartTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,20 @@ public class StopwatchActivity extends AppCompatActivity {
 		iv_sw_reset = findViewById(R.id.iv_sw_reset);
 		iv_sw_stop = findViewById(R.id.iv_sw_stop);
 		btn_sw_record = findViewById(R.id.btn_sw_record);
+        
+        iv_sw_start.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    myStartTime = SystemClock.elapsedRealtime();
+                    
+                }
+         });
+        
     }
+    String getTimeOut() {
+        long now = SystemClock.elapsedRealtime();
+        long outTime = now - myStartTime);
+        String time = String.format("%02d : %02d : %02d", (outTime / 1000 / 3600), (outTime / 1000 / 60) % 60, (outTime / 1000) % 60);
+        return time;
+	}
 }
