2a42d1c251b63d764c2ecb1e82e0d19c6760f2de
pby1360 <pby1360@gmail.com>
2020-04-02 00:06:21 +0900

    ToDo : 정지한 후 부터 다시 시작한 시간 메꾸기

--- app/src/main/java/com/example/training/StopwatchActivity.java
+++ app/src/main/java/com/example/training/StopwatchActivity.java
@@ -3,6 +3,7 @@ package com.example.training;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 import android.os.SystemClock;
 import android.view.View.*;
@@ -12,6 +13,7 @@ import android.os.Message;
 public class StopwatchActivity extends AppCompatActivity {
 	
 	private TextView tv_sw_time;
+    private TextView tv_sw_milSec;
 	private ImageView iv_sw_reset;
 	private ImageView iv_sw_start;
 	private ImageView iv_sw_stop;
@@ -19,12 +21,14 @@ public class StopwatchActivity extends AppCompatActivity {
     private long myStartTime;
 	private boolean isPlay=false;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
 		
 		tv_sw_time = findViewById(R.id.tv_sw_time);
+		tv_sw_milSec = findViewById(R.id.tv_sw_milSec);
 		iv_sw_start = findViewById(R.id.iv_sw_start);
 		iv_sw_reset = findViewById(R.id.iv_sw_reset);
 		iv_sw_stop = findViewById(R.id.iv_sw_stop);
@@ -35,7 +39,8 @@ public class StopwatchActivity extends AppCompatActivity {
                 public void onClick(View v) {
 					if (isPlay) {
 						myTimer.removeMessages(0);
-						isPlay = false
+                        iv_sw_start.setImageResource(R.drawable.icon_play_custom);
+						isPlay = false;
 					} else {
 						if (myStartTime == 0){
 							myStartTime = SystemClock.elapsedRealtime();
@@ -53,6 +58,7 @@ public class StopwatchActivity extends AppCompatActivity {
 	Handler myTimer = new Handler() {
 		public void handleMessage(Message msg) {
 			tv_sw_time.setText(getTimeOut());
+			tv_sw_milSec.setText(getMilSec());
 			myTimer.sendEmptyMessage(0);
 		}
 	};
@@ -62,4 +68,10 @@ public class StopwatchActivity extends AppCompatActivity {
         String time = String.format("%02d : %02d : %02d", (outTime / 1000 / 3600), (outTime / 1000 / 60) % 60, (outTime / 1000) % 60);
         return time;
 	}
+	String getMilSec() {
+        long now = SystemClock.elapsedRealtime();
+        long outTime = now - myStartTime;
+        String time = String.format("%02d", (outTime / 10) % 100);
+        return time;
+    }
 }
